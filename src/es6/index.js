bca0f0a183eea0c7ac082b0eb39292b6a6bcfcc0
artuknees <artuknees@gmail.com>
2022-05-07 16:54:52 -0300

    LET, CONST, multilinea, spread operator y desestructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -30,3 +30,71 @@ console.log(epicPhrase);
 // Con ES6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//////////////////////////////////////////
+
+// Multilinea en strings
+// Antes de ES6
+let lorem = "Vamos a inventar un texto bastante largo \n" + "para separar en dos parrafos.";
+// Esto queda separado en linea por el "\n"
+
+// Con ES6
+let lorem_2 = `Otra frase epica para separar
+solo con el enter tengo la frase dividida en parafos`;
+// Solo uso las comillas francesas y luego pongo enter sin mas detalles
+
+console.log(lorem);
+console.log(lorem_2);
+
+/////////////////////////////////////////
+
+// Desestructuración de elementos
+
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+
+// Antes de ES6
+console.log(person.name,person.age,person.country);
+
+// Con ES6
+let { name , age , country } = person;
+console.log(name , age , country);
+// Puedo extraer las variables que quiera del objeto para llamarlas mas facilmente.
+
+/////////////////////////////////
+
+// Operador de propagación
+
+let team_1 = ['Oscar','Julian','Ricardo'];
+let team_2 = ['Valeria','Jessica','Camila'];
+
+let education = ['David',...team_1,...team_2];
+
+console.log(education);
+
+
+/////////////////////////////////////////////
+
+// VAR, LET y CONST
+// LET
+{
+    var globalvar = "Global Var";
+}
+{
+    let globallet = "Global Let";
+    console.log(globallet);
+}
+console.log(globalvar);
+
+// Puedo acceder a global VAR porque realmente es global.
+// globallet es de bloque y queda solo dentro del bloque donde se definió
+
+
+// CONST
+// las variables CONST no permiten una reasignación
+const a = 'b';
+a = 'a';
+// Esto si puede hacerse con una variable LET o con una variable VAR.
