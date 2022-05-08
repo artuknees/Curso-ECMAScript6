dac4e04c2b0ae4c25b824da12b3df0b32ad20941
artuknees <artuknees@gmail.com>
2022-05-08 19:10:01 -0300

    qué se implementó en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+// Include ES7
+
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(7)){ // así es la sintaxis del include para preguntar si hay un 7 en numbers
+    console.log('Si, está el valor 7');
+} else {
+    console.log('No hay un 7');
+}
+// Da que si, está el 7
+
+
+// Elevar potencias ES7
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent; // asi se hace base potenciado a la exponente
+console.log(result);
